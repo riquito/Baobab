7740c818d3f1bba8b08e4a942882227a487f5d91
Riccardo Attilio Galli <riccardo@sideralis.org>
2010-06-26 00:28:32 +0200

    Added db connection to BaobabTest

--- src/test/BaobabTest.php
+++ src/test/BaobabTest.php
@@ -18,12 +18,31 @@
  * limitations under the License.
  */ 
 
-
-
-require_once 'PHPUnit/Framework.php';
+require_once('PHPUnit/Framework.php');
  
 class BaobabTest extends PHPUnit_Framework_TestCase {
+    protected static $db;
     
+    public static function setUpBeforeClass() {
+        require_once(dirname(__FILE__).DIRECTORY_SEPARATOR."conf_database.php");
+        if (!isset($DB_CONFIG)) self::fail("Missing or misconfigured conf_database.php");
+        
+        self::$db=@mysqli_connect(
+                      $DB_CONFIG["host"],
+                      $DB_CONFIG["username"],
+                      $DB_CONFIG["password"],
+                      $DB_CONFIG["dbname"],
+                      $DB_CONFIG["port"]);
+        
+        if (mysqli_connect_error()) {
+            self::fail(sprintf('Connect Error (%s): %s',mysqli_connect_errno(),mysqli_connect_error()));
+        }
+    }
+    
+    public static function tearDownAfterClass(){
+        mysqli_close(self::$db);
+        self::$db=NULL;
+    }
 }
 
 ?>
